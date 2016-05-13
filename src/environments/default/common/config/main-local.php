b8ce3c07de0cafc393a0e0d3a6d3177e8ed6962b
EKup <ekup73@gmail.com>
2016-05-13 16:46:26 +0400

    фикс

--- src/environments/default/common/config/main-local.php
+++ src/environments/default/common/config/main-local.php
@@ -3,12 +3,9 @@
 return [
     'components' => [
         'db' => [
-            'class' => 'yii\db\Connection',
             'dsn' => '#DB_TYPE#:host=#DB_HOST#;port=#DB_PORT#;dbname=#DB_NAME#',
             'username' => '#DB_LOGIN#',
             'password' => '#DB_PASSWORD#',
-            'charset' => 'utf8',
-            'enableSchemaCache' => !YII_DEBUG,
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
